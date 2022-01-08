70632f4156881061d21a9a76719adffe3fdb86fa
xuanchien01 <56905452+xuanchien01@users.noreply.github.com>
2022-01-08 22:38:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Hi",
-    introTitle: 'Chào Cậu',
+    introTitle: 'Hi!Chào Cậu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
